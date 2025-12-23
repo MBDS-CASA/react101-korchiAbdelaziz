d463b4a522109c65e7180a63f2a874cc2eccac47
korchiabdelaziz <abdelazizkorchi03@gmail.com>
2025-12-23 10:22:27 +0100

    TD01 : Ajouter un footer

---
+++ src/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer style={{
+            position: 'fixed',
+            bottom: 0,
+            width: '100%',
+            textAlign: 'center',
+            padding: '10px 0'
+        }}>
+            Tous droits réservés - Korchi Abdelaziz
+        </footer>
+    )
+}
+
+export default Footer
